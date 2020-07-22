a10b4168fcd82305469f5c3c566993caf6bfcd5c
Joyce <nokjo86@hotmail.com>
2020-07-22 11:26:46 +1000

    embedded ratings in location show method

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,23 +3,24 @@ class LocationsController < ApplicationController
   before_action :set_location, only: [:show, :update, :destroy]
 
   def index
-    rawData = Location.all.order(id: "desc").includes(:ratings)
-    @locations = rawData.map do |l|
+    rawLocation = Location.all.order(id: "desc").includes(:ratings)
+    @locations = rawLocation.map do |l|
       {id: l.id, name: l.name, description: l.description, tagline: l.tagline, address: l.address, longitude: l.longitude, latitude: l.latitude, ratings: l.ratings.average(:stars).to_i}
     end
     render json: @locations, status: 200
   end
 
   def show
-    rawData = Comment.includes(:user).where(location_id: @location.id)
-    if rawData.length==0 
+    location = {id: @location.id, name: @location.name, description: @location.description, tagline: @location.tagline, address: @location.address, longitude: @location.longitude, latitude: @location.latitude, ratings: @location.ratings.average(:stars).to_i}
+    rawComment = Comment.includes(:user).where(location_id: @location.id)
+    if rawComment.length==0 
       comments = [0]
     else 
-      comments = rawData.map do |c|
+      comments = rawComment.map do |c|
         {id: c.id, body: c.body, user: c.user.username, created_at: c.created_at, thread_id: c.thread_id}
       end
     end
-    render json: {location: @location, comments: comments}, status: 200
+    render json: {location: location, comments: comments}, status: 200
   end
 
   def create
