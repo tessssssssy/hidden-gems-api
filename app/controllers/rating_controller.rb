5191893b4f00661f90dd6c58c973aa5f24f4bbc0
Joyce <nokjo86@hotmail.com>
2020-07-22 11:30:25 +1000

    added rating controller and create method

--- app/controllers/rating_controller.rb
+++ app/controllers/rating_controller.rb
@@ -1,2 +1,17 @@
 class RatingController < ApplicationController
+  before_action :authenticate_user, only: [:create, :update, :destroy]
+  def create
+    ratings = current_user.ratings.create(ratings_params)
+    if ratings.errors.any?
+      render json: { errors: ratings.errors.full_messages }, status: :unprocessable_entity
+    else
+      render status: :created
+    end
+  end
+
+  private
+
+  def ratings_params
+    params.require(:ratings).permit(:stars, :location_id)
+  end
 end
