48ebfe61ea8db5b19088c1bcc331a5f3ccb2b7aa
tessssssssy <tessfabry@gmail.com>
2020-07-24 15:18:34 +1000

    changed photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,42 +1,45 @@
 class PhotosController < ApplicationController
     before_action :authenticate_user, only: [:create, :destroy]
     before_action :set_photo, only: [:show, :update, :destroy]
+
     def index
         photos = Photo.where(location_id: params[:location_id])
+        photos = generate_image_urls(photos)
+        render json: photos, status: 200
     end
 
     def show
+        render json: photo.merge({image: image.service_url})
     end
 
     def create
-        photo = Photo.new(photo_params)
-        if location.save
-            render json: { location: location, image: url_for(location.image) }, status: :created
+        byebug
+        photo = current_user.photos.new(photo_params)
+        if photo.save
+            render json: { photo: photo, image: url_for(photo.image) }, status: :created
           else
-            render json: { errors: location.errors.full_messages }, status: :unprocessable_entity
-          end
+            render json: { errors: photo.errors.full_messages }, status: :unprocessable_entity
+        end
     end
 
-    def delete
+    def destroy
+        photo.destroy
+        render json: "photo deleted", status: :ok
     end
 
     private
 
     def photo_params
-        params.require(:photo).permit(:user_id, :location_id, :image)
+        params.require(:photo).permit(:location_id, :image)
     end
     
     def set_photo
-        @location = Photo.find(params[:id])
+        photo = Photo.find(params[:id])
     end
 
-    def generate_image_urls(locations)
-        locations.map do |location|
-          if location.image.attached?
-            location.attributes.merge(image: url_for(location.image))
-          else
-            location
-          end
+    def generate_image_urls(photos)
+        photos.map do |photos|
+            photo.attributes.merge(image: url_for(photo.image))
         end
-      end
+    end
 end
